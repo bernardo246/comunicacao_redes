901c699623739850b024db350f46be2cea175091
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2026-08-26 11:48:39 -0300

    comentando as etapas do socket

--- server.py
+++ server.py
@@ -4,16 +4,14 @@
 # 4. PROCESSAR  → interpretar esses dados usando o protocol.py
 # 5. RESPONDER  → montar e enviar a resposta (o ACK do handshake)
 # 6. ENCERRAR   → fechar os sockets quando terminar
-
+import config
 import socket
 
-HOST = "localhost"
-PORTA = 5000
 
 def iniciar_servidor(host, porta):
-    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criando socket e definindo que é udp
+    servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # definindo a função
 
-    servidor.bind((host, porta))
-    servidor.listen(1)
+    servidor.bind((host, porta)) # definindo a porta e o ip do server
+    servidor.listen(1) # espera por rec
     return servidor
